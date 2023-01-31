99ccf0c06f0c72e4be485acf4f5ecf6997618672
David Sedlar <sedlardavid.cz@gmail.com>
2023-01-31 15:02:56 +0100

    Feature / email sender (#2)
    * email sender base
    
    * read email from template
    
    * cancel template
    
    * conditional email sending
    
    * removed test api

---
+++ app/Http/Controllers/EmailSendingController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+
+use SendinBlue\Client\Configuration;
+use SendinBlue\Client\Api\TransactionalEmailsApi;
+use GuzzleHttp;
+
+
+class EmailSendingController extends Controller
+{
+
+    private static function initialize()
+    {
+        $config = Configuration::getDefaultConfiguration()->setApiKey('api-key', env('SENDINBLUE_API_KEY'));
+
+        $apiInstance = new TransactionalEmailsApi(
+            // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
+            // This is optional, `GuzzleHttp\Client` will be used as default.
+            new GuzzleHttp\Client(),
+            $config
+        );
+
+        try {
+            $result = $apiInstance;
+            return $result;
+        } catch (Exception $e) {
+            echo 'Exception when calling instance$apiInstanceApi->getinstance$apiInstance: ', $e->getMessage(), PHP_EOL;
+        }
+    }
+
+
+    public static function sendEmail(EmailContent $type, $data)
+    {
+        $apiInstance = EmailSendingController::initialize();
+        $sendSmtpEmail = new \SendinBlue\Client\Model\SendSmtpEmail();
+        $sendSmtpEmail['subject'] = EmailSendingController::getEmailSubject($type);
+        $sendSmtpEmail['sender'] = array('name' => 'Ples UTB', 'email' => 'ples@sutb.cz');
+        $sendSmtpEmail['to'] = array(
+            array('email' => $data['reservation']->email, 'name' => $data['reservation']->name)
+        );
+
+        $content = EmailSendingController::getEmailContent($type, $data);
+
+        $sendSmtpEmail['htmlContent'] = $content;
+
+        try {
+            $result = $apiInstance->sendTransacEmail($sendSmtpEmail);
+            print_r($result);
+        } catch (Exception $e) {
+            echo 'Exception when calling TransactionalEmailsApi->sendTransacEmail: ', $e->getMessage(), PHP_EOL;
+        }
+    }
+
+    private static function getEmailContent(EmailContent $type, $data)
+    {
+        switch ($type) {
+            case EmailContent::Reserve:
+                $prepareContent = file_get_contents(dirname(__DIR__, 2) . '/View/Email/ReservationTemplate.htm');
+                $prepareContent = str_replace("{{count}}", count($data['seats']), $prepareContent);
+                //TODO
+                $prepareContent = str_replace("{{table}}", "x", $prepareContent);
+                $prepareContent = str_replace("{{endDate}}", $data['reservation']->created_at->addWeekDays(3)->format('d.m H:i'), $prepareContent);
+                return $prepareContent;
+            case EmailContent::Cancel:
+                return file_get_contents(dirname(__DIR__, 2) . '/View/Email/CancelTemplate.htm');
+        }
+    }
+    private static function getEmailSubject(EmailContent $type)
+    {
+        switch ($type) {
+            case EmailContent::Reserve:
+                return 'Reprezentační ples UTB 2023 - Potvrzení rezervace';
+            case EmailContent::Cancel:
+                return 'Reprezentační ples UTB 2023 - Zrušení rezervace';
+        }
+    }
+}
+
+enum EmailContent
+{
+    case Reserve;
+    case Cancel;
+}
