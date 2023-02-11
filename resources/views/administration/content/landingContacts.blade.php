f4028eee17dbfda4994eab591f1ff3eeaaae4ca6
David Sedlar <sedlardavid.cz@gmail.com>
2023-02-11 16:26:07 +0100

    Fix / bootstrap admin (#10)
    * rewritten nav, pages layout
    
    * commented navbar
    
    * login in bootstrap

--- resources/views/administration/content/landingContacts.blade.php
+++ resources/views/administration/content/landingContacts.blade.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -16,7 +18,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
 </head>
 
@@ -25,10 +27,8 @@
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
     <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
 
-
-
+        <x-navbar></x-navbar>
         <!-- Page Content -->
         <main>
 
@@ -86,5 +86,6 @@
 
 
 </body>
+
 
 </html>
