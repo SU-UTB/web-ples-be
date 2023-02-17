da0b4c64136f2054530569ad7de11dc907164890
SedlarDavid <sedlardavid.cz@gmail.com>
2023-02-17 21:50:02 +0100

    salons layout base

--- resources/views/salons.blade.php
+++ resources/views/salons.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Rezervacni system SUTB</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -16,7 +16,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  
+
 
 </head>
 
@@ -24,12 +24,82 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
-    <div class="min-h-screen bg-gray-100">
+    <div class="min-h-screen bg-gray-500">
 
         <!-- Page Content -->
         <main>
 
- 
+            <header style="height: 10vh; background-color:green">
+                Rezervacni system SU UTB
+            </header>
+
+            <div style="min-height: 85vh; ">
+                <form >
+                    <div
+                        style=" min-width: 100vw; display:flex; flex-direction: row; justify-content:center; align-items:center; box-sizing:content-box;">
+
+                        <div style="margin: 50px">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Salon</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Time</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Service</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+
+                            </div>
+
+                        </div>
+                        <div style="margin: 50px">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Name</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Phone</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp" placeholder="Enter email">
+
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
+                    <div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+
+                    </div>
+                </form>
+            </div>
+
+
+            <footer style="height: 5vh; background-color:red">
+                Vytvoreno SU
+
+            </footer>
         </main>
     </div>
 
