70a209b290a39cdeefd3bfacdee68f85daffc273
SedlarDavid <sedlardavid.cz@gmail.com>
2023-02-18 11:06:40 +0100

    additional tables added

--- app/Models/MakerService.php
+++ app/Models/MakerService.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SalonReservation extends Model
+class MakerService extends Model
 {
     use HasFactory;
 }
